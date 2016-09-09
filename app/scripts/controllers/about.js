aa2984fef008d6cc946ba4aa64f2c4e71e9a0c22
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-09-09 18:16:13 +0700

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pubTransApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pubTransApp
+ */
+angular.module('pubTransApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
